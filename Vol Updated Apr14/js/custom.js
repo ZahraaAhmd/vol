5bd4a6288ecd2ee0035a94f259e50c31d4b7feaf
zahraaAhmed <zahraa.ahmed.ham@gmail.com>
2020-04-28 21:43:08 +0200

    edits

--- Vol Updated Apr14/js/custom.js
+++ Vol Updated Apr14/js/custom.js
@@ -59,6 +59,34 @@ $(".partner-slide").slick({
         }
     ]
 });
+$(".event-slider").slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 800,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<button class="slick-prev"><img src="images/event-left.svg"></button>',
+    nextArrow: '<button class="slick-next"><img src="images/event-right.svg"></button>',
+
+    responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
 $("document").ready(function() {
     $(".accordion-heading").click(function() {
         $(this).parent(".accordion-group").toggleClass("active")
